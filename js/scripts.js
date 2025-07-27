1f032cd637f251c0fa058a749c2004d04579462f
anotechhub <anotechhub@gmail.com>
2025-07-27 21:36:34 +0700

    Initial commit: Menambahkan struktur awal landing page

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+// Inisialisasi Feather Icons
+feather.replace();
+
+// Logika untuk menu mobile
+const mobileMenuButton = document.getElementById('mobile-menu-button');
+const mobileMenu = document.getElementById('mobile-menu');
+
+mobileMenuButton.addEventListener('click', () => {
+    mobileMenu.classList.toggle('hidden');
+});
